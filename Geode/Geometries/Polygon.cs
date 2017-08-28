7fdb1900a5292da930aa4065b25d952db768d4d4
Taylor Hutchison <seanthutchison@gmail.com>
2017-08-27 21:22:15 -0500

    Implemented create feature for polygons types.

---
+++ Geode/Geometries/Polygon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geode.Geometries
+{
+    public class Polygon<T> : IGeoType
+    {
+        public string Type => "Polygon";
+        public IEnumerable<IEnumerable<T>> Coordinates { get; set; }
+        public Polygon(IEnumerable<IEnumerable<T>> coordinates)
+        {
+            Coordinates = coordinates;
+        }
+    }
+}
