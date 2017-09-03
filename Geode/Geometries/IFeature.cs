563d1cda5ac36aceee2bb6b37c85fbed5d56f45d
Taylor Hutchison <seanthutchison@gmail.com>
2017-09-03 12:51:25 -0500

    Feature now implements IFeature.

---
+++ Geode/Geometries/IFeature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geode.Geometries
+{
+    public interface IFeature<T>
+    {
+        string Type { get; }
+        T Geometry { get; set; }
+        IDictionary<string, object> Properties { get; set; }
+    }
+}
