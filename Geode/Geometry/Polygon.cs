49770ceda40f148896e138bc0bbc7db79626557f
Taylor Hutchison <seanthutchison@gmail.com>
2017-09-04 20:02:24 -0500

    Add non-generic polygon type that uses double type by default.

--- Geode/Geometry/Polygon.cs
+++ Geode/Geometry/Polygon.cs
@@ -4,6 +4,16 @@ using System.Text;
 
 namespace Geode.Geometry
 {
+    public class Polygon: IGeoType
+    {
+        public string Type => "Polygon";
+        public IEnumerable<IEnumerable<double>> Coordinates { get; set; }
+        public Polygon(IEnumerable<IEnumerable<double>> coordinates)
+        {
+            Coordinates = coordinates;
+        }
+    }
+
     public class Polygon<T> : IGeoType
     {
         public string Type => "Polygon";
