e85fc2b538ae336f44d90460549f5dd75ffcffd8
Taylor Hutchison <seanthutchison@gmail.com>
2017-09-08 07:45:00 -0500

    Add GeometryServiceTests file and update csproj info.

---
+++ Geode.Tests/GeometryServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Geode.Geometry;
+using Geode.Services;
+
+namespace Geode.Tests
+{
+    public class GeometryServiceTests
+    {
+        [Fact]
+        public void GetCentroidOfListOfPositions()
+        {
+            IEnumerable<IPosition> points = new List<IPosition>
+            {
+                new Point(0,0,0),
+                new Point(1,1,10)
+            };
+            var centroid = points.Centroid();
+            Assert.Equal(0.5, centroid.Position[0]);
+            Assert.Equal(0.5, centroid.Position[1]);
+            Assert.Equal(5, centroid.Position[2]);
+        }
+    }
+}
