1e814afc1910de1c0569ff2f435ae50938963525
Taylor Hutchison <seanthutchison@gmail.com>
2017-09-09 00:42:37 -0500

    Deleted generic Serializer. Created GeoJson specific serializer.

---
+++ Geode/Serializers/GeoJsonSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Geode.Geometry;
+
+namespace Geode.Serializers
+{
+    public static class GeoJsonSerializer
+    {
+        public static string ToGeoJson(this IFeature<IGeoType> feature, bool indented = false)
+        {
+            var formatting = indented ? Formatting.Indented : Formatting.None;
+            return JsonConvert.SerializeObject(feature, formatting);
+        }
+
+        public static string ToGeoJson(this IFeatureCollection<IFeature<IGeoType>> featureCollection, bool indented = false)
+        {
+            var formatting = indented ? Formatting.Indented : Formatting.None;
+            return JsonConvert.SerializeObject(featureCollection, formatting);
+        }
+    }
+}
