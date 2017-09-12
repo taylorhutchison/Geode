fd4bc828a7642bc451112eaba4b4babbc510a222
Taylor Hutchison <seanthutchison@gmail.com>
2017-09-12 15:23:56 -0500

    Started QuadTree structure.

---
+++ Geode/Structures/QuadTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Geode.Geometry;
+
+namespace Geode.Structures
+{
+    public class QuadTree
+    {
+        public QuadTree(IEnumerable<IPosition> positions)
+        {
+
+        }
+
+        public QuadTree Add(IPosition position)
+        {
+            return this;
+        }
+        
+        public QuadTree Add(IEnumerable<IPosition> position)
+        {
+            return this;
+        }
+    }
+}
