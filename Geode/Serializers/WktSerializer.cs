70496d906b880be4db374936ec5fbdbc6312baa2
Taylor Hutchison <seanthutchison@gmail.com>
2017-09-28 09:54:17 -0500

    Started work on well-known-text transforms.

---
+++ Geode/Serializers/WktSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Geode;
+using Geode.Geometry;
+
+namespace Geode.Serializers
+{
+    public class WktSerializer
+    {
+        public static string ToWKT(IPosition position)
+        {
+            if(position == null)
+            {
+                return string.Empty;
+            }
+            return $"POINT ({position.X} {position.Y})";
+        }
+    }
+}
