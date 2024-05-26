88f0816c7a9c985f05ccafd55ab45151f5cdd2fb
Taylor Hutchison <seanthutchison@gmail.com>
2024-05-26 17:13:06 -0500

    Cleanup midpoint file

--- src/Geode.Tests/Algorithms/MidPointTests.cs
+++ src/Geode.Tests/Algorithms/MidPointTests.cs
@@ -14,7 +14,7 @@ namespace Geode.Tests.Algorithms
         public void LineStringWithOnePosition()
         {
             var line = new double[][] {
-                new double[] { 2, 3 }
+                [2, 3]
             }.ToLineString();
             var midPoint = line.GetMidPoint();
             Assert.Equal(2, midPoint.Position[0]);
@@ -25,8 +25,8 @@ namespace Geode.Tests.Algorithms
         public void LineStringWithTwoPositions()
         {
             var line = new double[][] {
-                new double[] { 0, 0 },
-                new double[] { 1, 1 }
+                [ 0, 0 ],
+                [ 1, 1 ]
             }.ToLineString();
             var midPoint = line.GetMidPoint();
             Assert.Equal(0.5, midPoint.Position[0]);
@@ -37,9 +37,9 @@ namespace Geode.Tests.Algorithms
         public void LineStringWithThreePositions()
         {
             var line = new double[][] {
-                new double[] { 0, 0 },
-                new double[] { 1, 1 },
-                new double[] { 5, 5 }
+                [0, 0],
+                [1, 1],
+                [5, 5]
             }.ToLineString();
             var midPoint = line.GetMidPoint();
             Assert.Equal(2.5, midPoint.Position[0]);
