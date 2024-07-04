1f28275840ea887688e0c069c5641c5352711d4c
Taylor Hutchison <seanthutchison@gmail.com>
2024-07-04 13:26:01 -0500

    Distance calculation tests

--- src/Geode.Tests/AlgorithmsTests/DistanceTests.cs
+++ src/Geode.Tests/AlgorithmsTests/DistanceTests.cs
@@ -34,6 +34,32 @@ public class DistanceTests
                 new(15, 5)
             }.ToPolyline();
         var distance = line.GetDistance();
-        Assert.Equal(21.2132034355965m, distance);
+        Assert.Equal(21.2132034355965m, distance, 12);
+    }
+
+    [Fact]
+    public void DistanceCalculatedFor3DPolyline()
+    {
+        var line = new Point3D[] {
+                new(0, 0, 0),
+                new(5, 5, 5),
+                new(10, 0, 10),
+                new(15, 5, 15)
+            }.ToPolyline();
+        var distance = line.GetDistance();
+        Assert.Equal(25.980762113533175m, distance, 12);
+    }
+
+    [Fact]
+    public void DistanceCalculatedFor3DPolylineWithoutZAxisChange()
+    {
+        var line = new Point3D[] {
+                new(0, 0, 0),
+                new(5, 5, 0),
+                new(10, 0, 0),
+                new(15, 5, 0)
+            }.ToPolyline();
+        var distance = line.GetDistance();
+        Assert.Equal(21.2132034355965m, distance, 12);
     }
 }
